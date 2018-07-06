cd1f1c9267e77673a2d9b0f939fc13e2ff45a8f0
Swastik Shrivastava <swastikshrivastava2@gmail.com>
2018-07-06 12:28:38 +0530

    Add files via upload

---
+++ encrypt.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function()
+{
+var aa=$("#importantlinks").val();
+if (aa == null) {
+    window.location.href = "https://www.important.gq/";
+};
+ $("#importantlinks").attr("href","https://www.important.gq/");
+
+
+});
